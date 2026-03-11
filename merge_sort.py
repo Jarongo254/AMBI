f6c43c5138666cf8b3a3eb5e62da789e174396ea
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2026-03-11 22:38:59 +0100

    Added merge_sort.py + updated README.md

---
+++ merge_sort.py
@@ -0,0 +1,12 @@
+def merge(U, V, T):
+    pass
+
+def merge_sort(T):
+    pass
+
+def main():
+    T = [None] + list(map(int, input("Enter your array to sort, space separated: ").split()))
+    print("Sorted array:", merge_sort(T))
+
+if __name__ == "__main__":
+    main()
