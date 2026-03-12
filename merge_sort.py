83fbd48c89eceb76d5ce97c222236f06634c9321
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2026-03-12 08:49:53 +0100

    updated merge_sort.py + updated README.md

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,39 @@
+import math
+from insert_sort import insert
+
+
 def merge(U, V, T):
-    pass
+    i=0
+    j=0
+    m = len(U)
+    n = len(V)
+    U.append(float('inf'))
+    V.append(float('inf'))
+    for k in range(0, m+n):
+        if U[i] < V[j]:
+            T[k] = U[i]
+            i += 1
+        else:
+            T[k] = V[j]
+            j += 1
+    return T
 
 def merge_sort(T):
-    pass
+    n = len(T)
+    if n < 5:
+        return insert(T)
+
+    else:
+        U = T[0:n//2+1]
+        V = T[n//2 + 1:n]
+        merge_sort(U)
+        merge_sort(V)
+        merge(U, V, T)
+    return T
+
 
 def main():
-    T = [None] + list(map(int, input("Enter your array to sort, space separated: ").split()))
+    T = list(map(int, input("Enter your array to sort, space separated: ").split()))
     print("Sorted array:", merge_sort(T))
 
 if __name__ == "__main__":
