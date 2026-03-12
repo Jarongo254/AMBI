7d3b0140f732300c4ec8dc9b3e61087b495d2711
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2026-03-12 15:21:57 +0100

    added quick_sort.py + readme

---
+++ quick_sort.py
@@ -0,0 +1,9 @@
+def pivot(T, var_l): # var_l should be returned by something???
+    pass
+
+def quick_sort(T):
+    pass
+
+def main():
+    T = list(map(int, input("Enter your array to sort, space separated: ").split()))
+    print("Sorted array:", quick_sort(T))
