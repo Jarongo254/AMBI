7ffe10d997a6dc28ed0be05f24a402a434fd9885
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2026-03-12 18:52:16 +0100

    updated quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,9 +1,34 @@
-def pivot(T, var_l): # var_l should be returned by something???
-    pass
+def pivot(T,i,j): # var_l should be returned by something???
+    p = T[i]            #Using first element as pivot
+    k = i
+    l = j + 1
+    while True:
+        k = k+1
+        while k <= j and T[k] < p:
+            k = k + 1
+        l = l - 1
+        while T[l] > p:
+            l = l - 1
+        if k >= l:
+            break
 
-def quick_sort(T):
-    pass
+        T[k], T[l] = T[l], T[k]
+    T[i], T[l] = T[l], T[i]     # Place pivot in correct position
+    return l             # Return partition index
+
+def quick_sort(T, i, j):
+    if i < j:
+        l = pivot(T, i, j)
+        quick_sort(T, i, l - 1)
+        quick_sort(T, l + 1, j)
+    return T
 
 def main():
     T = list(map(int, input("Enter your array to sort, space separated: ").split()))
-    print("Sorted array:", quick_sort(T))
+    i=0
+    j= len(T) - 1
+    print("Sorted array:", quick_sort(T,i,j))
+
+if __name__ == "__main__":
+    main()
+
