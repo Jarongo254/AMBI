8a9bf3833393767829cf9b441f4d232d3886136e
Sidney Omenya <123540913+SidneyOmenya@users.noreply.github.com>
2026-03-17 16:14:21 +0100

    updated DP_change.py

--- DP_change.py
+++ DP_change.py
@@ -37,40 +37,47 @@ function backtrack(N)
             j <- j - d[i]
 '''
 def change(N):
-    D = [100, 25, 10, 5, 1]            # coin denominations
+    D = [100, 25, 10, 5, 1]   # coin denominations
     n = len(D)
-    C= [[1,n],[0,N]]     # DP matrix
 
-    for i in range(1, n):
-        C[i,0] = 0           # amount 0 needs 0 coins
+    # DP matrix: n rows, N+1 columns
+    C = [[0]*(N+1) for _ in range(n)]
 
-    for i in range(1, n):
-        for j in range(1, N):
-            if i == 1 and j < D[1]:
-                C[i,j] = 10000
+    # First column = 0
+    for i in range(n):
+        C[i][0] = 0
 
-            elif i == 1:
-                C[i,j] = 1 + C[1, j - D[1]]
+    for i in range(n):
+        for j in range(1, N+1):
+
+            if i == 0 and j < D[0]:
+                C[i][j] = float('inf')
+
+            elif i == 0:
+                C[i][j] = 1 + C[0][j - D[0]]
 
             elif j < D[i]:
-                C[i,j] = C[i-1,j]
+                C[i][j] = C[i-1][j]
 
             else:
-                C[i,j] = min( C[i-1,j],
-                               1 + C[i, j - D[i]] )
-    return C, C[n,N]
+                C[i][j] = min(C[i-1][j],
+                              1 + C[i][j - D[i]])
+
+    return C, C[n-1][N]
 
 def backtrack(N):
-    D = [100, 25, 10, 5, 1]            # coin denominations
-    C, v = change(N)
-    i = len(D)
+    D = [100, 25, 10, 5, 1]
+    C, _ = change(N)
+
+    i = len(D) - 1
     j = N
+
     while j > 0:
-        if i > 1 and C[i,j] == C[i-1,j]:
-            i = i - 1    # No coin of denom i needed
+        if i > 0 and C[i][j] == C[i-1][j]:
+            i = i - 1
         else:
-            print(f"coin of denomination {D[i]} used") # coin i used
-            j <- j - D[i]
+            print(f"coin of denomination {D[i]} used")
+            j = j - D[i]
 
 def main():
     N = int(input("Enter amount of change to pay"))
